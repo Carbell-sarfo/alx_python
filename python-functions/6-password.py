fd2aa30da10d8767911ba3284df93b8375594e79
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-07-28 15:24:22 +0000

    password check

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -24,24 +24,3 @@ def validate_password(password):
         return False
 
     return True
-Explanation:
-
-The validate_password function takes a password password as input.
-It checks if the password is at least 8 characters long using the len() function.
-It uses a loop to check if the password contains at least one uppercase letter, one lowercase letter, and one digit using the isupper(), islower(), and isdigit() string methods, respectively.
-If the password doesn't meet any of the criteria, the function returns False; otherwise, it returns True.
-You can use this function to validate a password like this:
-
-python
-Copy code
-result = validate_password("Password123")
-print(result)  # Output: True
-
-result = validate_password("weak")
-print(result)  # Output: False
-The function will return True if the password passes all the checks and False otherwise.
-
-
-
-
-
