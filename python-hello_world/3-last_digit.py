5708d15e9522465bb722221242b0b9b768d8d856
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-07-28 09:46:42 +0000

    fixing negative output

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,8 +7,7 @@ def status ():
         return ("and is greater than 5")
     elif (number % 10 == 0): 
         return ("and is 0")
-    elif (number % 10 < 6 and number % 10 != 0):
+    elif (number < 0 and number % 10 < 6 and number % 10 != 0):
         return ("and is less than 6 and not 0") 
 
 print("Last digit of", number, "is" , number % 10, status())
-
