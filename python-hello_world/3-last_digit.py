328fe542d88f21d74ad6fe5e2fe4a9fd860d1f36
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-07-28 13:05:59 +0000

    negative issue fiexed

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,12 +2,18 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-def status ():
-    if (number % 10 > 5):
-        return ("and is greater than 5")
-    elif (number % 10 == 0): 
-        return ("and is 0")
-    elif (number < 0 and number % 10 < 6 and number % 10 != 0):
-        return ("and is less than 6 and not 0") 
 
-print("Last digit of", number, "is" , number % 10, status())
+last_digit = abs(number) % 10
+
+if number < 0:
+    print("Last digit of", number, "is", -abs(last_digit), end=" ")
+elif number > 0:
+    print("Last digit of", number, "is", last_digit, end=" ")
+   
+# message if something is wrong
+if last_digit > 5 and number > 0:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
