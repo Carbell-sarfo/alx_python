abd2744e78e18a01385cb421a3e3471d8196161e
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-02 13:35:18 +0000

    arguments corrected

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     num_arguments = len(arguments)
 
-    print(f"Number of argument(s): {num_arguments}{'s' if num_arguments != 1 else ''}:")
-    print("." if num_arguments == 0 else "")
+    print(f"1 argument:" if num_arguments == 1 else f"{num_arguments} arguments:")
 
     for i, arg in enumerate(arguments, start=1):
         print(f"{i}: {arg}")
