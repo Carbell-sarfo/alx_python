2c5d280ee713822c38b01378cebf25c2c0a4f689
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-02 12:15:41 +0000

    task one for python modules

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2
+
+# Import the function from add_0.py
+from add_0 import add
+
+# Print the result of the addition using string formatting
+print(f"{a} + {b} = {add(a, b)}")
---
+++ python-import_modules/README.md
@@ -0,0 +1,7 @@
+# Python - import & modules
+
+This is a continuation of the python class.
+
+Here are looking at what modules are and working with such modules.
+
+
