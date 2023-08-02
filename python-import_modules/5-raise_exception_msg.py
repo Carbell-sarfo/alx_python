8858ff406f973477155b3df919d78ff4ccba29c6
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-02 14:48:43 +0000

    error c is fun

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,9 +1,2 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
-
-
-# Call the function to raise the exception
-try:
-    raise_exception_msg("C is fun")
-except NameError as e:
-    print(e)
