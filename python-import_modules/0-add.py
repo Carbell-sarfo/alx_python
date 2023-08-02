c7baf8e0508b39a7d763588271f00c5af9a7bdd4
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-02 13:03:23 +0000

    remove all comments

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,3 @@
-# a = 1
-# b = 2
-
-# # Import the function from add_0.py
-# from add_0 import add
-
-# # Print the result of the addition using string formatting
-# print(f"{a} + {b} = {add(a, b)}")
-
 if __name__ == "__main__":
     from add_0 import add
 
@@ -15,5 +6,4 @@ if __name__ == "__main__":
 
     result = add(a, b)
 
-    # print(f"{a} + {b} = {result}")
     print("{} + {} = {}".format(a, b, result))
