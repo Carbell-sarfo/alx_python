3002454d9540ed48281efcf7e5a814e0006e8252
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-02 12:57:10 +0000

    task one fixing

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,18 @@
-a = 1
-b = 2
+# a = 1
+# b = 2
 
-# Import the function from add_0.py
-from add_0 import add
+# # Import the function from add_0.py
+# from add_0 import add
 
-# Print the result of the addition using string formatting
-print(f"{a} + {b} = {add(a, b)}")
+# # Print the result of the addition using string formatting
+# print(f"{a} + {b} = {add(a, b)}")
+
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+    result = add(a, b)
+
+    print(f"{a} + {b} = {result}")
