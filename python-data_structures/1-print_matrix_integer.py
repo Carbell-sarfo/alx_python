7a9ac59fce27c85e4a464ad7283484cffd5f715f
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-06 12:05:35 +0000

    empty matrix should be one character long

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,10 @@
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i, element in enumerate(row):
-            if i == len(row) - 1:
-                print("{:d}".format(element))
-            else:
-                print("{:d}".format(element), end=' ')
+    if not matrix:
+        print(" ")
+    else:
+        for row in matrix:
+            for i, element in enumerate(row):
+                if i == len(row) - 1:
+                    print("{:d}".format(element))
+                else:
+                    print("{:d}".format(element), end=' ')
