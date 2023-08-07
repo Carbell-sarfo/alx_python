0173c6bbd8e9caa27ded64c60d450833f79a93e1
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-07 12:01:06 +0000

    0-squares

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,19 @@
-# -*- coding: utf-8 -*-
 class Square:
-    '''A class Square that defines a square.'''
+    """
+    This class represents a square shape.
+
+    Attributes:
+        __size (int): Private instance attribute to store the size of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a Square object with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Note:
+            The size parameter is assigned to the private instance attribute __size.
+        """
         self.__size = size
