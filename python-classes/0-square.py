bc1e8a5535f4252e22de186dba10bc9bc660f5d6
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-07 10:07:37 +0000

    adding square

---
+++ python-classes/0-square.py
@@ -0,0 +1,16 @@
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square (private attribute).
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance with the given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
