d9a0955bb965d0f7c44e4ae39997197c943162ca
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-07 10:12:06 +0000

    module was documented

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,4 @@
+# a class Square that defines a square
 class Square:
-    """
-    A class representing a square.
-
-    Attributes:
-        __size (int): The size of the square (private attribute).
-    """
-
     def __init__(self, size):
-        """
-        Initialize a Square instance with the given size.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
