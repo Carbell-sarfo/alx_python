720a2bfcaa670d712defa581fb6b6fbfe644b9fb
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-08-18 16:12:42 +0000

    3 -squares

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,7 +1,14 @@
 class Square:
+    """
+    This class defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    
     def __init__(self, size=0):
         """
-        Initialize a Square instance with an optional size.
+        Initializes a Square instance.
 
         Args:
             size (int, optional): The size of the square. Defaults to 0.
@@ -11,21 +18,21 @@ class Square:
             ValueError: If size is less than 0.
         """
         self.size = size
-
+    
     @property
     def size(self):
         """
-        Retrieve the size of the square.
+        Getter method for retrieving the size of the square.
 
         Returns:
             int: The size of the square.
         """
         return self.__size
-
+    
     @size.setter
     def size(self, value):
         """
-        Set the size of the square.
+        Setter method for setting the size of the square.
 
         Args:
             value (int): The size of the square.
@@ -36,17 +43,15 @@ class Square:
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-
         if value < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = value
-
+    
     def area(self):
         """
-        Calculate and return the area of the square.
+        Computes and returns the area of the square.
 
         Returns:
             int: The area of the square.
         """
-        return self.__size * self.__size
+        return self.__size ** 2
