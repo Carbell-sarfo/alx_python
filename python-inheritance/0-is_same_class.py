bb680a1ed484c39340905a0f9c3eadd1976dda80
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-22 15:19:35 +0000

    is kind of the same

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,21 +1,16 @@
 """
-This module defines Same class or inherit from.
+This module defines a function to check if an object is exactly an instance of the specified class.
 """
 
-"""
-is kind of class function
-"""
-
-def is_kind_of_class(obj, a_class):
+def is_same_class(obj, a_class):
     """
-    Check if the given object is an instance of, or if it is an instance of a class
-    that inherited from, the specified class.
+    Check if the given object is exactly an instance of the specified class.
 
     Args:
         obj: The object to be checked.
         a_class: The class to compare against.
 
     Returns:
-        True if obj is an instance of a_class or its subclass; otherwise, False.
+        bool: True if obj is an instance of a_class; otherwise, False.
     """
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
