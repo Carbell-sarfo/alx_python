8c1b6905dbc94ef25e17d779846d4cafbc5aba83
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-22 18:36:47 +0000

    8 square

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -2,7 +2,35 @@
 This module defines a class Square that inherits from Rectangle.
 """
 
-from models.rectangle import Rectangle
+class Rectangle:
+    """
+    The Rectangle class represents a rectangle shape with a width and height.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            ValueError: If width or height is less than or equal to 0.
+        """
+        if width <= 0 or height <= 0:
+            raise ValueError("Width and height must be positive integers.")
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """
+        Computes and returns the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.width * self.height
 
 class Square(Rectangle):
     """
@@ -11,7 +39,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """
-        Initializes a Square instance.
+        Initializes a Square instance with size.
 
         Args:
             size (int): The size of the square.
@@ -19,14 +47,15 @@ class Square(Rectangle):
         Raises:
             ValueError: If size is less than or equal to 0.
         """
-        self.integer_validator("size", size)
+        if size <= 0:
+            raise ValueError("Size must be a positive integer.")
         super().__init__(size, size)
 
-    def __str__(self):
+    def area(self):
         """
-        Returns a string representation of the Square object.
+        Computes and returns the area of the square.
 
         Returns:
-            str: A string in the format "[Square] <width/height>".
+            int: The area of the square.
         """
-        return "[Square] {}/{}".format(self.width, self.height)
+        return self.width * self.width
