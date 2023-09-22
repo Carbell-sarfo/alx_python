69a9cfae1498219905b97c10d4f212e6927670fa
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-22 15:24:37 +0000

    geometory

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,10 @@
+"""
+This module defines a base geometry class.
+"""
+
+class BaseGeometry:
+    """
+    The BaseGeometry class is a base class for geometric shapes.
+    It doesn't have any methods or attributes at the moment.
+    """
+    pass
