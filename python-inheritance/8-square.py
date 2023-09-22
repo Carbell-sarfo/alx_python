50afc5d3787fd0f11ad7a7c4321e9a52119b0efa
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-22 23:02:45 +0000

    8 once again

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -4,7 +4,7 @@ This module defines a class Square that inherits from Rectangle.
 
 class Rectangle:
     """
-    The Rectangle class represents a rectangle shape with a width and height.
+    The Rectangle class represents a rectangle shape with width and height.
     """
 
     def __init__(self, width, height):
@@ -18,6 +18,24 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def area(self):
+        """
+        Computes and returns the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.width * self.height
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+
+        Returns:
+            str: A string in the format "[Rectangle] width/height".
+        """
+        return "[Rectangle] {}/{}".format(self.width, self.height)
+
 class Square(Rectangle):
     """
     The Square class represents a square shape with equal side lengths.
@@ -30,4 +48,14 @@ class Square(Rectangle):
         Args:
             size (int): The size of the square, which is used for both width and height.
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            str: A string in the format "[Square] size/size".
+        """
+        return "[Square] {}/{}".format(self.width, self.height)
