01a9078a2d2e8241ac287ba26f2bf3096ac749c9
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-22 23:00:15 +0000

    8 again

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -14,27 +14,13 @@ class Rectangle:
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
-
-        Raises:
-            ValueError: If width or height is less than or equal to 0.
         """
-        if width <= 0 or height <= 0:
-            raise ValueError("Width and height must be positive integers.")
         self.width = width
         self.height = height
 
-    def area(self):
-        """
-        Computes and returns the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
-        return self.width * self.height
-
 class Square(Rectangle):
     """
-    The Square class represents a square shape with a single side length 'size'.
+    The Square class represents a square shape with equal side lengths.
     """
 
     def __init__(self, size):
@@ -42,20 +28,6 @@ class Square(Rectangle):
         Initializes a Square instance with size.
 
         Args:
-            size (int): The size of the square.
-
-        Raises:
-            ValueError: If size is less than or equal to 0.
+            size (int): The size of the square, which is used for both width and height.
         """
-        if size <= 0:
-            raise ValueError("Size must be a positive integer.")
         super().__init__(size, size)
-
-    def area(self):
-        """
-        Computes and returns the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.width * self.width
