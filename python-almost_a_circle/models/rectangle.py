84b446d1af72bbacf2105ded11c253654900793b
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-23 00:11:38 +0000

    rectangle redone 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,99 +1,80 @@
-"""
-This module defines a class Rectangle that inherits from the Base class.
-"""
+""" Rectangle Module """
 
 from models.base import Base
 
 class Rectangle(Base):
-    """
-    Rectangle class, which inherits from the Base class.
+    """ Rectangle class inheriting from Base """
 
-    This class represents rectangles with private instance attributes for width, height, x, and y.
-    The use of private attributes with getters and setters helps protect and validate these attributes.
-
-    Attributes:
-        None
-
-    Methods:
-        __init__(self, width, height, x=0, y=0, id=None): Constructor for the Rectangle class.
-
-    Private Instance Attributes:
-        __width (int): Width of the rectangle.
-        __height (int): Height of the rectangle.
-        __x (int): X-coordinate of the upper-left corner of the rectangle.
-        __y (int): Y-coordinate of the upper-left corner of the rectangle.
-
-    Public Attributes:
-        width (int): Width of the rectangle.
-        height (int): Height of the rectangle.
-        x (int): X-coordinate of the upper-left corner of the rectangle.
-        y (int): Y-coordinate of the upper-left corner of the rectangle.
-        id (int): Identifier of the rectangle object.
-
-    """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Constructor for the Rectangle class.
+        """ Constructor for Rectangle class """
 
-        Args:
-            width (int): Width of the rectangle.
-            height (int): Height of the rectangle.
-            x (int, optional): X-coordinate of the upper-left corner of the rectangle (default is 0).
-            y (int, optional): Y-coordinate of the upper-left corner of the rectangle (default is 0).
-            id (int, optional): Identifier of the rectangle object (default is None).
+        # Call the constructor of the Base class with id
+        super().__init__(id)
 
-        """
-        super().__init__(id)  # Call the constructor of the Base class with 'id'
+        # Validate and assign width
         self.width = width
+
+        # Validate and assign height
         self.height = height
+
+        # Validate and assign x
         self.x = x
+
+        # Validate and assign y
         self.y = y
 
-        # Private attributes with getters and setters
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        """ Getter for width """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """ Setter for width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        """ Getter for height """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """ Setter for height """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            return self.__x
+    @property
+    def x(self):
+        """ Getter for x """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            if not isinstance(value, int):
-                raise TypeError("x must be an integer")
-            if value < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """ Setter for x """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            return self.__y
+    @property
+    def y(self):
+        """ Getter for y """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            if not isinstance(value, int):
-                raise TypeError("y must be an integer")
-            if value < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """ Setter for y """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
